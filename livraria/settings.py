a76a117e7ee6f35acb01e2d6bff76a24cc85808f
PedroLuccaLuciano <pedrolucca.luciano@gmail.com>
2022-12-14 08:26:22 -0300

    aula 19

--- livraria/settings.py
+++ livraria/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "rest_framework_simplejwt",
     "rest_framework",
+    "drf_spectacular",
     "uploader",
     'core'
 ]
@@ -122,6 +123,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 # Default primary key field type
@@ -139,6 +141,12 @@ MEDIA_URL = "http://localhost:8000/media/"
 MEDIA_ENDPOINT = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 FILE_UPLOAD_PERMISSIONS = 0o640
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Livraria API",
+    "DESCRIPTION": "API para gerenciamento de livraria, incluindo endpoints e documentação.",
+    "VERSION": "1.0.0",
+}
 
 
 AUTH_USER_MODEL = "core.Usuario"
